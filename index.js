ed8649b3776c3e7625bba2dd68ce93dd4961622c
Muhammad Ari Al Ghifari <muhammad.ghifari@technosoftautomotive.com>
2026-09-16 19:46:12 +0700

    feat: add simple hello world website
    - index.js serves a Hello World page on SERVER_PORT (default 8080)
    - package.json with start script for convenience

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const PORT = process.env.SERVER_PORT || 8080;
+const HOST = process.env.SERVER_HOST || '0.0.0.0';
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}/`);
+});
