82abc0deb741213decb5226301a6c494375338b4
Muhammad Ari Al Ghifari <56450870+alfari24@users.noreply.github.com>
2026-09-16 19:53:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const HOST = process.env.SERVER_HOST || '0.0.0.0';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello World</h1>');
+  res.end('<h1>Hello World, Test 1</h1>');
 });
 
 server.listen(PORT, HOST, () => {
